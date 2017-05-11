b4c83e31de4bd2963211dc2990d0133721ef40ef
Arman Yessymkanov <arman.yessymkanov@gmail.com>
2017-05-12 01:54:05 +0600

    Removed trash reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,6 @@
 import {combineReducers} from 'redux'
-import {addTodoReducer} from './addTodoReducer'
 import {todolistReducer} from './todolistReducer'
 
 export const reducer = combineReducers({
-    addTodoReducer,
     todolistReducer
 })
